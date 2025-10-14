0fd906228ef4782b30e4979c375062548b515d90
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-14 20:47:37 +0200

    implementing GET + POST on place

---
+++ part2/app/api/v1/places.py
@@ -0,0 +1,70 @@
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('places', description='Place operations')
+
+# Define the place model for input validation and documentation
+place_model = api.model('Place', {
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
+})
+
+
+@api.route('/')
+class PlaceList(Resource):
+    @api.expect(place_model)
+    @api.response(201, 'Place successfully created')
+    @api.response(400, 'Invalid input data')
+    def post(self):
+        """Register a new place"""
+        place_data = api.payload
+        new_place = facade.create_place(place_data)
+        return {
+            "id": new_place.id,
+            'title': new_place.title,
+            'description': new_place.description,
+            'price': new_place.price,
+            'latitude': new_place.latitude,
+            'longitude': new_place.longitude,
+            'owner_id': new_place.owner,
+        }, 201
+
+    @api.response(200, 'List of places retrieved successfully')
+    def get(self):
+        """Retrieve a list of all places"""
+        all_places = facade.get_all_places()
+        return [
+            {
+                "id": all_places_items.id,
+                "title": all_places_items.name,
+                "latitude": all_places_items.latitude,
+                "longitude": all_places_items.longitude
+            }
+            for all_places_items in all_places
+        ], 200
+
+@api.route('/<place_id>')
+class PlaceResource(Resource):
+    @api.response(200, 'Place details retrieved successfully')
+    @api.response(404, 'Place not found')
+    def get(self, place_id):
+        """Get place details by ID"""
+        place = facade.get_place(place_id)
+        if not place:
+            return {'error': 'Place not found'}, 404
+        ####### return a checker pcq doit inclure les amenities
+
+
+    @api.expect(place_model)
+    @api.response(200, 'Place updated successfully')
+    @api.response(404, 'Place not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, place_id):
+        """Update a place's information"""
+        # Placeholder for the logic to update a place by ID
+        pass
