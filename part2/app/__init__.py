83e0633f86ec17dbb725b5e9d6bb2c84d2d6ee74
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-14 16:08:20 +0200

    adding namespace for amenities

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
@@ -9,4 +10,5 @@ def create_app():
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added later
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     return app
