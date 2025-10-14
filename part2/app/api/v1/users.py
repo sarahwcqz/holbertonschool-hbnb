eef5748ca987a3f90de67154dc39a2eced6e22b1
chermat mustapha <mchermat@outlook.fr>
2025-10-14 12:40:35 +0200

    add get for get the list of all users

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -1,6 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
-
+from flask import jsonify
 api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
@@ -27,3 +27,29 @@ class UserList(Resource):
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+
+    def get(self):
+        """Retrieve a List of Users"""
+        all_users = facade.get_all()
+        return [
+            {
+                'id': all_users_items.id,
+                'first_name': all_users_items.first_name,
+                'last_name': all_users_items.last_name,
+                'email': all_users_items.email
+            }
+            for all_users_items in all_users
+        ], 200
+    
+
+@api.route('/<user_id>')
+class UserResource(Resource):
+    @api.response(200, 'User details retrieved successfully')
+    @api.response(404, 'User not found')
+    def get(self, user_id):
+        """Get user details by ID"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+    
