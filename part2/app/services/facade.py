815d6c0f0dec1cc79760291958f060ed39879995
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-14 19:19:09 +0200

    adding function for amenities in facade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
@@ -9,7 +10,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
+# ----------------------- USER -------------------------
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -28,3 +29,18 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+# ------------------------- AMENITY --------------------
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        return self.amenity_repo.update(amenity_id, amenity_data)
