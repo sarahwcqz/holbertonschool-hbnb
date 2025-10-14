6ecc721d1ca8821fd0aa3139f8bcb4bda1fbf77c
chermat mustapha <mchermat@outlook.fr>
2025-10-14 10:01:59 +0200

    add condition

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -3,11 +3,23 @@ from .BaseModel import BaseModel
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
-        self.title = title
+        if len(title) < 101:
+            self.title = title
+        else:
+            raise ValueError("Title must be less than 100 charaters")
         self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
+        if price >= 0:
+            self.price = price
+        else:
+            raise ValueError("Price must be positive")
+        if -90.0 <latitude < 90.0:
+            self.latitude = latitude
+        else:
+            raise ValueError("Latitude must be in range of -90.0 to 90.0")
+        if -180 <longitude< 180:
+            self.longitude = longitude
+        else:
+            raise ValueError("Longitude must be in range of -180 to 180")
         self.owner = owner
         self.reviews = []  # List to store related reviews
         self.amenities = []  # List to store related amenities
--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -2,8 +2,14 @@ from .BaseModel import BaseModel
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
+        if len(first_name) < 51:
+            self.first_name = first_name
+        else:
+            raise ValueError("first name must be less than 50 characters")
+        if len(last_name) < 51:
+            self.last_name = last_name
+        else:
+            raise ValueError("last name must be less than 50 characters")
         self.email = email
         self.is_admin = is_admin
         self.places = []
