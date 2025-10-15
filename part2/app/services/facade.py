f0857181888ea6e8f746df25d81a42b2a5899ba6
chermat mustapha <mchermat@outlook.fr>
2025-10-15 16:40:09 +0200

    add _id to place and user

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -2,6 +2,7 @@ from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
+from app.models.review import Review
 
 
 class HBnBFacade:
@@ -60,3 +61,24 @@ class HBnBFacade:
         ######### pas sure que ca marche cette affaire vu
         ######### qu'il y en a potentiellement plsr
         return self.place_repo.update(place_id, place_data)
+    
+# ------------------------- Review --------------------
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+        return self.review_repo.get_by_attribute('place', place_id)
+
+    def update_review(self, review_id, review_data):
+        return self.review_repo.update(review_id, review_data)
+
+    def delete_review(self, review_id):
+        return self.review_repo.delete(review_id)
