5b0d601283f661df7a79d146a15d2d519ef40709
chermat mustapha <mchermat@outlook.fr>
2025-10-16 17:47:28 +0200

    add try/except for PUT methode if invalid input data

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -70,7 +70,16 @@ class UserResource(Resource):
             #on charge le user present dans la DB
         updated_user = api.payload
             #on update les champs
-        user_inDB.first_name = updated_user.get('first_name', user_inDB.first_name)
-        user_inDB.last_name = updated_user.get('last_name', user_inDB.last_name)
-        user_inDB.email = updated_user.get('email', user_inDB.email)
+        try:
+            user_inDB.first_name = updated_user.get('first_name', user_inDB.first_name)
+        except:
+            return {"error": "Invalid input data"}, 400
+        try:
+            user_inDB.last_name = updated_user.get('last_name', user_inDB.last_name)
+        except:
+            return {"error": "Invalid input data"}, 400
+        try:
+            user_inDB.email = updated_user.get('email', user_inDB.email)
+        except:
+            return {"error": "Invalid input data"}, 400
         return {'id': user_inDB.id, 'first_name': user_inDB.first_name, 'last_name': user_inDB.last_name, 'email': user_inDB.email}, 200
