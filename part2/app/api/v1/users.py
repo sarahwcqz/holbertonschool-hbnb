79ee040cb8cedecd8f1f349f2975a18822c613a5
chermat mustapha <mchermat@outlook.fr>
2025-10-16 16:00:46 +0200

    add try/execpt for create user for invalid input data

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -22,10 +22,14 @@ class UserList(Resource):
 
         # Simulate email uniqueness check (to be replaced by real validation with persistence)
         existing_user = facade.get_user_by_email(user_data['email'])
+
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
-        new_user = facade.create_user(user_data)
+        try:
+            new_user = facade.create_user(user_data)
+        except:
+            return {"error": "Invalid input data"}, 400
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 
     def get(self):
