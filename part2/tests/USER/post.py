c4fce4df247af68e1973c18a67cb3f18e66b258b
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-17 12:54:28 +0200

    adding separated files for user tests post, put, get

---
+++ part2/tests/USER/post.py
@@ -0,0 +1,66 @@
+import unittest
+from app import create_app
+
+
+
+class TestUserEndpoints(unittest.TestCase):     # creation class test (herite module test)
+
+    def setUp(self):    ### init l'env de test 
+        self.app = create_app()     # appli flask
+        self.client = self.app.test_client()    # simule un navigateur pou client HTTP (permet les requetes sans lancer reellement le serveur)
+        
+
+################################################# POST #############################################
+
+    def test_create_valid(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Barack",
+            "last_name": "Obama",
+            "email": "TheKing@WhiteHouse.com"
+        })
+        self.assertEqual(response.status_code, 201) # format: (resultat obtenu, resultat attendu)
+
+
+    def test_empty_name(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "",
+            "last_name": "Obama",
+            "email": "TheQueen@WhiteHouse.com"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    
+    def test_empty_surname(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Michelle",
+            "last_name": "",
+            "email": "TheQueen@WhiteHouse.com"
+        })
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_invalid_mail_format(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Michelle",
+            "last_name": "Obama",
+            "email": "INVALIDE_MAIL"
+        })
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_long_name(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Michellebutmynameissolongitisbarelypossibletoreaditihadsomuchpainlearningitwheniwasakid",
+            "last_name": "Obama",
+            "email": "TheQueen@WhiteHouse.com"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_long_surname(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Michelle",
+            "last_name": "Obamabutitisthesameproblemewithmysurnameidontknowwhodoesthisbutishastostoptobehonnesticanttakeitanymore",
+            "email": "TheQueen@WhiteHouse.com"
+        })
+        self.assertEqual(response.status_code, 400)
+
