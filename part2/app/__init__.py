acfdc18902c173dd528d7cef73eb09751c4e68dd
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-27 09:46:17 +0100

    puting back to normal to part2

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,9 +5,9 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as review_ns
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)        ## need to from config import DevelopmentConfig ??
+
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
