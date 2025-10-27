53dde3ac5140ed0aa558d9e55a9fdcb1379272eb
chermat mustapha <mchermat@outlook.fr>
2025-10-27 10:28:59 +0100

    add password

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -6,13 +6,14 @@ regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{
 
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False,):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
         self.places = []
+        self.hash_password(password)
 
 
     @property
