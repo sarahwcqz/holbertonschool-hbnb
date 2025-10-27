cb1ce4b5b25fe37ab351f80dbffe940be364551f
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-27 09:54:00 +0100

    starting structure for part3

---
+++ part3/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restx import Api
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as review_ns
+
+def create_app(config_class="config.DevelopmentConfig"):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+
+    # Placeholder for API namespaces (endpoints will be added later)
+    # Additional namespaces for places, reviews, and amenities will be added later
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(review_ns, path='/api/v1/reviews')
+    return app
