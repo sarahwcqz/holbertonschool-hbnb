76a142e5586efeeacdf55e2885be29ff46ec2abc
chermat mustapha <mchermat@outlook.fr>
2025-10-30 15:01:20 +0100

    add authentication for post an amenity

--- part3/app/api/amenities.py
+++ part3/app/api/amenities.py
@@ -1,6 +1,8 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
 from app.extensions import db
+from flask_jwt_extended import jwt_required, get_jwt
+
 
 api = Namespace('amenities', description='Amenity operations')
 
@@ -14,8 +16,13 @@ class AmenityList(Resource):
     @api.expect(amenity_model)
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
+    @jwt_required
     def post(self):
         """Register a new amenity"""
+        current_user = get_jwt()
+        if not current_user.get("is_admin"):
+            return {'error': 'Admin privileges required'}, 403
+        
         amenity_data = api.payload
         try:
             new_amenity = facade.create_amenity(amenity_data)
